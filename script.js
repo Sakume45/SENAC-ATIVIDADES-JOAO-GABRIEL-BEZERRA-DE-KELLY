de2ee41457d88fbec1a0c6f13f8c36e6ff727252
Sakume45 <138834177+Sakume45@users.noreply.github.com>
2025-09-25 11:05:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let numeroSecreto;
+let contadorDeChances = 1;
+const MAX_CHANCES = 10;
+
+const campoPalpite = document.querySelector('#campoPalpite');
+const botaoEnviarPalpite = document.querySelector('#botaoEnviarPalpite');
+const botaoReiniciar = document.querySelector('#botaoReiniciar')
+
+const mensagemElement = document.querySelector('.mensagem');
+const paplpitesAnterioresElement = document.querySelector('.palpites-anteriores');
+const dicaElement = document.querySelector('.dica');
+const chancesRestantesElement = document.querySelector('#chancesRestantes');
+
+let listaDePalpites = [];
+
+function iniciarJogo() {
+    numeroSecreto = Math.floor(Math.random() * 100) + 1;
+    console.log('Número Secreto(apenas para teste):', numeroSecreto);
+}
